4813002ef93f8dbb700d740bd37d0493ed0452b6
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2024-06-20 18:29:42 +0200

    feat: reimplement using the brand new botica-lib-java

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM openjdk:11
-
-WORKDIR /app
-COPY target/fishbowl.jar /app/fishbowl.jar
-
-CMD ["java", "-jar", "/app/fishbowl.jar"]
--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,2 +1,3 @@
 ./mvnw clean install
-docker build -t fishbowl .
+docker build -t fishbowl_fish fishbowl-fish
+docker build -t fishbowl_manager fishbowl-manager
